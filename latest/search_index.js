62f742ae7a9d645b3c12871563def2e87c281ab7
autodocs <autodocs>
2018-05-15 09:09:19 +0000

    build based on 258f4f3

--- latest/search_index.js
+++ latest/search_index.js
@@ -941,7 +941,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Reference",
     "title": "MathOptInterface.ConstraintPrimal",
     "category": "type",
-    "text": "ConstraintPrimal(N)\nConstraintPrimal()\n\nThe assignment to the constraint primal values in result N. If N is omitted, it is 1 by default.\n\n\n\n"
+    "text": "ConstraintPrimal(N)\nConstraintPrimal()\n\nThe assignment to the constraint primal values in result N. If N is omitted, it is 1 by default.\n\nGiven a constraint function-in-set, the ConstraintPrimal is the value of the function evaluated at the primal solution of the variables. For example, given the constraint ScalarAffineFunction([x,y], [1, 2], 3)-in-Interval(0, 20) and  a primal solution of (x,y) = (4,5), the ConstraintPrimal solution of the  constraint is 1 * 4 + 2 * 5 + 3 = 17.\n\n\n\n"
 },
 
 {
