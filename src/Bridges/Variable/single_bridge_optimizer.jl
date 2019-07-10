ae357ad20eaab43edd52fc9757e409324170fed5
Benoît Legat <benoit.legat@gmail.com>
2019-07-10 14:15:55 +0200

    Fix names with SingleVariable constraints

--- src/Bridges/Variable/single_bridge_optimizer.jl
+++ src/Bridges/Variable/single_bridge_optimizer.jl
@@ -16,10 +16,14 @@ mutable struct SingleBridgeOptimizer{BT<:AbstractBridge, OT<:MOI.ModelLike} <: M
     map::Map # index of bridged variable -> variable bridge
     var_to_name::Dict{MOI.VariableIndex, String}
     name_to_var::Union{Dict{String, MOI.VariableIndex}, Nothing}
+    con_to_name::Dict{MOI.ConstraintIndex, String}
+    name_to_con::Union{Dict{String, MOI.ConstraintIndex}, Nothing}
 end
 function SingleBridgeOptimizer{BT}(model::OT) where {BT, OT <: MOI.ModelLike}
     SingleBridgeOptimizer{BT, OT}(
-        model, Map(), Dict{MOI.VariableIndex, String}(), nothing)
+        model, Map(),
+        Dict{MOI.VariableIndex, String}(), nothing,
+        Dict{MOI.ConstraintIndex, String}(), nothing)
 end
 
 function bridges(bridge::MOI.Bridges.AbstractBridgeOptimizer)
