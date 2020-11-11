aefbc3c808e4b0fd8a981e8f7e25e74505ba4b62
Shadi Akiki <shadiakiki1986@gmail.com>
2020-11-11 08:45:49 +0200

    feedback from blegat: identity function simplification, no negation of coefficient, F and G are the same

--- src/Bridges/Constraint/ltgt_to_interval.jl
+++ src/Bridges/Constraint/ltgt_to_interval.jl
@@ -1,25 +1,21 @@
 # The code here is mostly copied from the flip_sign.jl code for FlipSignBridge and GreaterToLessBridge
 
 """
-    AbstractToIntervalBridge{T, S1, F, G}
+    AbstractToIntervalBridge{T, S1, S2, F}
 
-Bridge a `G`-in-`S1` constraint into an `F`-in-`Interval` constraint by multiplying
-the function by `+1` and taking the point reflection of the set across the
-origin. The flipped `F`-in-`S` constraint is stored in the `constraint`
+Bridge a `F`-in-`GreaterThan` or `F`-in-`LessThan` constraint into an `F`-in-`Interval` constraint by
+using Inf or -Inf for the upper or lower limit respectively. The `F`-in-`S` constraint is stored in the `constraint`
 field by convention.
 """
 abstract type AbstractToIntervalBridge{
     T, S1<:MOI.AbstractSet, S2<:MOI.AbstractSet,
-    F<:MOI.AbstractFunction, G<:MOI.AbstractFunction} <: SetMapBridge{T, S2, S1, F, G} end
+    F<:MOI.AbstractFunction} <: SetMapBridge{T, S2, S1, F, F} end
 
-# The function map is the identity
-map_function(::Type{<:AbstractToIntervalBridge{T}}, func) where {T} = MOIU.operate(+, T, func)
-# The map is an involution
-inverse_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = map_function(BT, func)
-# The map is symmetric
-adjoint_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = map_function(BT, func)
-# The map is a symmetric involution
-inverse_adjoint_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = map_function(BT, func)
+# The function map is the identity. It is also an involution, symmetric, and a symmetric involution.
+map_function(::Type{<:AbstractToIntervalBridge{T}}, func) where {T} = func
+inverse_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = func
+adjoint_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = func
+inverse_adjoint_map_function(BT::Type{<:AbstractToIntervalBridge}, func) = func
 
 function MOI.delete(model::MOI.ModelLike, bridge::AbstractToIntervalBridge, i::IndexInVector)
     func = MOI.get(model, MOI.ConstraintFunction(), bridge.constraint)
@@ -35,53 +31,49 @@ function MOI.modify(model::MOI.ModelLike, bridge::AbstractToIntervalBridge,
                     change::MOI.ScalarCoefficientChange)
     MOI.modify(
         model, bridge.constraint,
-        MOI.ScalarCoefficientChange(change.variable, -change.new_coefficient))
+        MOI.ScalarCoefficientChange(change.variable, change.new_coefficient))
 end
 function MOI.modify(model::MOI.ModelLike, bridge::AbstractToIntervalBridge,
                     change::MOI.MultirowChange{T}) where T
-    new_coefficients = Tuple{Int64, T}[
-        (index, -coef) for (index, coef) in change.new_coefficients]
     MOI.modify(model, bridge.constraint,
                MOI.MultirowChange(change.variable,
-                                  new_coefficients))
+                                  change))
 end
 
 """
-    GreaterToIntervalBridge{T, F<:MOI.AbstractScalarFunction, G<:MOI.AbstractScalarFunction} <:
+    GreaterToIntervalBridge{T, F<:MOI.AbstractScalarFunction} <:
         AbstractToIntervalBridge{T, MOI.GreaterThan{T}}
 
-Transforms a `G`-in-`GreaterThan{T}` constraint into an `F`-in-`Interval{T}`
+Transforms a `F`-in-`GreaterThan{T}` constraint into an `F`-in-`Interval{T}`
 constraint.
 """
-struct GreaterToIntervalBridge{T, F<:MOI.AbstractScalarFunction, G<:MOI.AbstractScalarFunction} <:
-    AbstractToIntervalBridge{T, MOI.GreaterThan{T}, MOI.Interval{T}, F, G}
+struct GreaterToIntervalBridge{T, F<:MOI.AbstractScalarFunction} <:
+    AbstractToIntervalBridge{T, MOI.GreaterThan{T}, MOI.Interval{T}, F}
     constraint::CI{F, MOI.Interval{T}}
 end
 map_set(::Type{<:GreaterToIntervalBridge}, set::MOI.GreaterThan) = MOI.Interval(set.lower, Inf)
 inverse_map_set(::Type{<:GreaterToIntervalBridge}, set::MOI.Interval) = MOI.GreaterThan(set.lower)
 function concrete_bridge_type(::Type{<:GreaterToIntervalBridge{T}},
-                              G::Type{<:MOI.AbstractScalarFunction},
+                              F::Type{<:MOI.AbstractScalarFunction},
                               ::Type{MOI.GreaterThan{T}}) where T
-    F = MOIU.promote_operation(+, T, G)
-    return GreaterToIntervalBridge{T, F, G}
+    return GreaterToIntervalBridge{T, F}
 end
 
 """
-    LessToIntervalBridge{T, F<:MOI.AbstractScalarFunction, G<:MOI.AbstractScalarFunction} <:
+    LessToIntervalBridge{T, F<:MOI.AbstractScalarFunction} <:
         AbstractToIntervalBridge{T, MOI.LessThan{T}}
 
-Transforms a `G`-in-`LessThan{T}` constraint into an `F`-in-`Interval{T}`
+Transforms a `F`-in-`LessThan{T}` constraint into an `F`-in-`Interval{T}`
 constraint.
 """
-struct LessToIntervalBridge{T, F<:MOI.AbstractScalarFunction, G<:MOI.AbstractScalarFunction} <:
-    AbstractToIntervalBridge{T, MOI.LessThan{T}, MOI.Interval{T}, F, G}
+struct LessToIntervalBridge{T, F<:MOI.AbstractScalarFunction} <:
+    AbstractToIntervalBridge{T, MOI.LessThan{T}, MOI.Interval{T}, F}
     constraint::CI{F, MOI.Interval{T}}
 end
 map_set(::Type{<:LessToIntervalBridge}, set::MOI.LessThan) = MOI.Interval(-Inf, set.upper)
 inverse_map_set(::Type{<:LessToIntervalBridge}, set::MOI.Interval) = MOI.LessThan(set.upper)
 function concrete_bridge_type(::Type{<:LessToIntervalBridge{T}},
-                              G::Type{<:MOI.AbstractScalarFunction},
+                              F::Type{<:MOI.AbstractScalarFunction},
                               ::Type{MOI.LessThan{T}}) where T
-    F = MOIU.promote_operation(+, T, G)
-    return LessToIntervalBridge{T, F, G}
+    return LessToIntervalBridge{T, F}
 end
