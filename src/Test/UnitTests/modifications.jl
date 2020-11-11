5f6d31789de8794c45cbb4e7118065b782abadea
Shadi Akiki <shadiakiki1986@gmail.com>
2020-11-11 11:20:42 +0000

    ltgt1 pass tests

--- src/Test/UnitTests/modifications.jl
+++ src/Test/UnitTests/modifications.jl
@@ -96,13 +96,13 @@ end
 modificationtests["solve_transform_singlevariable_lessthan"] = solve_transform_singlevariable_lessthan
 
 """
-    solve_set_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConfig)
+    solve_set_scalaraffine_lessthan2(model::MOI.ModelLike, config::TestConfig)
 
 Test modifying set of ScalarAffineFunction-in-LessThan constraint. If
 `config.solve=true` confirm that it solves correctly, and if
 `config.duals=true`, check that the duals are computed correctly.
 """
-function solve_set_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConfig)
+function solve_set_scalaraffine_lessthan2(model::MOI.ModelLike, config::TestConfig)
     MOI.empty!(model)
     MOIU.loadfromstring!(model,"""
         variables: x
@@ -115,7 +115,7 @@ function solve_set_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConfi
         objective_value   = 1.0,
         variable_primal   = [(x, 1.0)],
         constraint_primal = [(c, 1.0)],
-        constraint_dual   = [(c, -1.0)]
+        constraint_dual   = [(c, 1.0)] # <<< difference from above
     )
     MOI.set(model, MOI.ConstraintSet(), c, MOI.LessThan(2.0))
     @test MOI.get(model, MOI.ConstraintSet(), c) == MOI.LessThan(2.0)
@@ -123,10 +123,10 @@ function solve_set_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConfi
         objective_value   = 2.0,
         variable_primal   = [(x, 2.0)],
         constraint_primal = [(c, 2.0)],
-        constraint_dual   = [(c, -1.0)]
+        constraint_dual   = [(c, 1.0)] # <<< difference from above
     )
 end
-modificationtests["solve_set_scalaraffine_lessthan"] = solve_set_scalaraffine_lessthan
+modificationtests["solve_set_scalaraffine_lessthan2"] = solve_set_scalaraffine_lessthan2
 
 
 """
@@ -160,6 +160,38 @@ function solve_coef_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConf
     )
 end
 modificationtests["solve_coef_scalaraffine_lessthan"] = solve_coef_scalaraffine_lessthan
+
+"""
+    solve_coef_scalaraffine_lessthan2(model::MOI.ModelLike, config::TestConfig)
+
+Test modifying a variable coefficient in a ScalarAffineFunction-in-LessThan
+constraint. If `config.solve=true` confirm that it solves correctly, and if
+`config.duals=true`, check that the duals are computed correctly.
+"""
+function solve_coef_scalaraffine_lessthan2(model::MOI.ModelLike, config::TestConfig)
+    MOI.empty!(model)
+    MOIU.loadfromstring!(model,"""
+        variables: x
+        maxobjective: 1.0x
+        c: 1.0x <= 1.0
+    """)
+    x = MOI.get(model, MOI.VariableIndex, "x")
+    c = MOI.get(model, MOI.ConstraintIndex, "c")
+    test_model_solution(model, config;
+        objective_value   = 1.0,
+        variable_primal   = [(x, 1.0)],
+        constraint_primal = [(c, 1.0)],
+        constraint_dual   = [(c, 1.0)] # <<< difference from above
+    )
+    MOI.modify(model, c, MOI.ScalarCoefficientChange(x, 2.0))
+    test_model_solution(model, config;
+        objective_value   = 0.5,
+        variable_primal   = [(x, 0.5)],
+        constraint_primal = [(c, 1.0)],
+        constraint_dual   = [(c, 0.5)] # <<< difference from above
+    )
+end
+modificationtests["solve_coef_scalaraffine_lessthan2"] = solve_coef_scalaraffine_lessthan2
 
 """
     solve_func_scalaraffine_lessthan(model::MOI.ModelLike, config::TestConfig)
